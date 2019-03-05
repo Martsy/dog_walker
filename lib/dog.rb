1b0cb713e74920d304e3fbd0e2ff50f90c169b33
Martha Troubh <indexer@icloud.com>
2019-03-04 20:34:09 -0700

    added attributes

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,12 @@ class Dog
 
   attr_reader :name, :age, :breed, :small_breed
 
-  def initialize(name, age, breed, walks)
+  def initialize(name, age, breed, small_breed)
     @name        = name
     @age         = age
     @breed       = breed
     @small_breed = small_breed
   end
+
+
 end
--- test/dog_test.rb
+++ test/dog_test.rb
@@ -11,4 +11,24 @@ class PetTest < Minitest::Test
   def test_pet_class_exists
     assert_instance_of Dog, @sodie
   end
+
+  def test_pet_has_a_name
+    assert_equal "Max", @max.name
+    assert_equal "Sodie", @sodie.name
+  end
+
+  def test_pet_has_an_age
+    assert_equal 11, @max.age
+    assert_equal 9, @sodie.age
+  end
+
+  def test_pet_has_a_breed
+    assert_equal "Shih-Tzu", @sodie.breed
+    assert_equal "Cocker Spaniel", @max.breed
+  end
+
+  def test_if_pet_is_a_small_breed
+    assert_equal true, @sodie.small_breed
+    assert_equal false, @max.small_breed
+  end
 end
